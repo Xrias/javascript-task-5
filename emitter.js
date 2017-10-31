bf1337f0e0035e0ee53b0628ae3ca8efcb0fb6cc
Reshetnikov Artem <mr.artem.reshetnikov.2017@mail.ru>
2017-10-31 19:01:44 +0500

    1

--- emitter.js
+++ emitter.js
@@ -4,14 +4,47 @@
  * Сделано задание на звездочку
  * Реализованы методы several и through
  */
-getEmitter.isStar = true;
+getEmitter.isStar = false;
 module.exports = getEmitter;
+
+/**
+* Выполнить событие для студентов, которые на него подписаны
+* @param {Array} studentsWithFunc
+*/
+function funcForStudents(studentsWithFunc) {
+    for (let i = 0; i < studentsWithFunc.length; i++) {
+        studentsWithFunc[i].func();
+    }
+}
+
+/**
+* Преобразовать событие вида в xxx.yyy в [xxx.yyy, xxx]
+* @param {String} event
+* @returns {Array}
+*/
+function eventToKeys(event) {
+    let keys = [];
+    let event_ = event;
+    if (event_.indexOf('.') === -1) {
+        return [event_];
+    }
+    for (let i = event.split('.').length; i > 0; i--) {
+        keys[event.split('.').length - i] = event_;
+        let arr = event_.split('.');
+        arr.pop();
+        event_ = arr.join('.');
+    }
+
+    return keys;
+}
 
 /**
  * Возвращает новый emitter
  * @returns {Object}
  */
 function getEmitter() {
+    var events = {};
+
     return {
 
         /**
@@ -19,26 +52,51 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object}
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            if (!(event in events)) {
+                events[event] = [];
+            }
+            events[event].push({
+                student: context,
+                func: handler.bind(context)
+            });
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object}
          */
         off: function (event, context) {
-            console.info(event, context);
+            let eventsForDelete = Object.keys(events).filter(
+                storedEvent => storedEvent.indexOf(event + '.') === 0 || event === storedEvent);
+            eventsForDelete.forEach(eventForDelete => {
+                events[eventForDelete] = events[eventForDelete].filter(
+                    subscriber => subscriber.student !== context);
+            });
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object}
          */
         emit: function (event) {
-            console.info(event);
+            let keys = eventToKeys(event);
+            for (let j = 0; j < keys.length; j++) {
+                if (!(events[keys[j]] === undefined)) {
+                    funcForStudents(events[keys[j]]);
+                }
+            }
+
+            return this;
         },
 
         /**
